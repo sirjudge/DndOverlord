2a6a098a3b5b5c210e7eae18cf8a842192683274
nico <nico.a.judge@gmail.com>
2023-06-11 11:21:15 -0400

    updated gitignore + added todo in diceRoller.js

--- src/DndOverlordHomePage/wwwroot/js/DiceRoller.js
+++ src/DndOverlordHomePage/wwwroot/js/DiceRoller.js
@@ -6,7 +6,8 @@ function Roll(){
     let numberOfDice = $("#numberOfDiceToRoll").val();
     let numberOfSides = $("#diceSides").val();
     let rollTotal = 0;
-    for (let i = 0; i <numberOfDice ; i ++){
+    for (let i = 0; i < numberOfDice ; i ++){
+        //todo: this can roll 0, figure out how to make it not roll 0
         let numRoller = Math.floor(Math.random() * numberOfSides);
         rollTotal += numRoller;
     }
