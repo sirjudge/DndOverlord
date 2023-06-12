baecff695b6c4d2f4253626936e358deec7a9248
nico <nico.a.judge@gmail.com>
2023-06-11 22:47:02 -0400

    updated character controller and repository to add get character and get character list logic + added base IRepository class with ToDataTable method

--- src/DndOverlordHomePage/Repositories/CharacterRepository.cs
+++ src/DndOverlordHomePage/Repositories/CharacterRepository.cs
@@ -4,14 +4,14 @@ using System.Data.SqlClient;
 namespace DndOverlordHomePage.Repositories;
 using DndOverlordHomePage.Models;
 
-public class CharacterRepository
+public class CharacterRepository : IRepository
 {
 
     public void Insert(Character character) => Insert(new List<Character>() { character });
     
     public void Insert(List<Character> characters)
     {
-        
+        var characterTable = ToDataTable<Character>(characters);
     }
 
     public void Delete()
@@ -24,6 +24,27 @@ public class CharacterRepository
         
     }
 
+    public List<Character> GetCharacterList()
+    {
+        using (var sqlConnection = new SqlConnection(GetConnectionString()))
+        {
+            sqlConnection.Open();
+            var storedProcedureName = "GetCharacterList_01";
+            using (var sqlCommand = new SqlCommand(storedProcedureName, sqlConnection))
+            {
+                sqlCommand.CommandTimeout = 30;
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                using (var reader = sqlCommand.ExecuteReader())
+                {
+                    var characterList = new List<Character>();
+                    while (reader.Read())
+                        characterList.Add(new Character(sqlCommand.ExecuteReader()));
+                    return characterList;
+                }
+            }
+        }
+    }
+    
     public Character Get(long characterId)
     {
         using (var sqlConnection = new SqlConnection(GetConnectionString()))
